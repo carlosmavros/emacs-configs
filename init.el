2c80f72a2d6797508d9d9bb8145af2efa3de8044
carlosmavros <carlosmavros@gmail.com>
2021-12-03 21:12:20 +0200

    added jedi

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 (setq coding-system-for-read 'utf-8 )	
 (setq coding-system-for-write 'utf-8 )
 (setq sentence-end-double-space nil)	
-(setq default-fill-column 80)		
+(setq default-fill-column 85)		
 (setq initial-scratch-message "Welcome to Emacs") 
 
 ;; UI tweaks
@@ -120,7 +120,7 @@
    "b s" 'save-buffer
    "b p" 'previous-buffer
    "b k" 'kill-this-buffer
-   "b b" 'list-buffers
+   "b b" 'switch-to-buffer
    "b e" '(eval-buffer :which-key "buffer") 
 
    ; files
@@ -426,10 +426,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-ivy lsp-treemacs lsp-ui lsp-mode which-key visual-fill-column use-package rainbow-delimiters python-mode org-bullets general evil eshell-git-prompt doom-themes doom-modeline counsel-projectile)))
+   '(jedi company-jedi lsp-ivy lsp-treemacs lsp-ui lsp-mode which-key visual-fill-column use-package rainbow-delimiters python-mode org-bullets general evil eshell-git-prompt doom-themes doom-modeline counsel-projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;; Jedi
+(use-package jedi
+  :hook (python-mode . jedi:setup)) 
